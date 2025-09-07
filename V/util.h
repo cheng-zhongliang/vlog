f52b50fc794913ee27a0ae03b91e508f5e72bada
cheng-zhongliang <codelogic@163.com>
2025-09-07 23:00:31 +0800

    V

---
+++ V/util.h
@@ -0,0 +1,31 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <limits.h>
+
+/* Saturating addition: overflow yields UINT_MAX. */
+inline unsigned int sat_add(unsigned int x, unsigned int y) {
+    return x + y >= x ? x + y : UINT_MAX;
+}
+
+/* Saturating subtraction: underflow yields 0. */
+inline unsigned int sat_sub(unsigned int x, unsigned int y) {
+    return x >= y ? x - y : 0;
+}
+
+/* Saturating multiplication: overflow yields UINT_MAX. */
+inline unsigned int sat_mul(unsigned int x, unsigned int y) {
+    return (!y ? 0 : x <= UINT_MAX / y ? x * y : UINT_MAX);
+}
+
+#ifndef MIN
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+#endif
+
+#ifndef MAX
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+#endif
+
+#define ARRAY_SIZE(ARRAY) (sizeof ARRAY / sizeof *ARRAY)
+
+#endif
